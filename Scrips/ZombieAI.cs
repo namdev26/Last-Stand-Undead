4601a5465f3757285b3d95f3a42421b491fa563c
namdev26 <vuhoangnam310104@gmail.com>
2025-01-10 02:26:39 +0700

    10/1 2h30

---
+++ Scrips/ZombieAI.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class ZombieAI : MonoBehaviour
+{
+    public Seeker seeker;
+    public float moveSpeed;
+    public float nextWaypointDistance;
+    public SpriteRenderer zombieSR;
+    public Transform target;
+
+    private Coroutine moveCoroutine;
+    private Path path;
+
+    private void Start()
+    {
+        target = FindObjectOfType<Player>().gameObject.transform;
+
+        InvokeRepeating("CaculatorPath", 0f, 0.5f);
+    }
+
+    void CaculatorPath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(transform.position, target.position, OnPathCallback);
+        }
+    }
+
+    void OnPathCallback(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+
+            if (moveCoroutine != null)
+            {
+                StopCoroutine(moveCoroutine);
+            }
+
+            moveCoroutine = StartCoroutine(MoveToTargetCoroutine());
+        }
+        else
+        {
+            Debug.LogWarning("Pathfinding error: " + p.errorLog);
+        }
+    }
+
+    IEnumerator MoveToTargetCoroutine()
+{
+    int currentWP = 0;
+
+    if (path == null || path.vectorPath == null || path.vectorPath.Count == 0)
+    {
+        Debug.LogWarning("No path available for movement!");
+        yield break;
+    }
+
+    while (currentWP < path.vectorPath.Count)
+    {
+        Vector2 direction = ((Vector2)path.vectorPath[currentWP] - (Vector2)transform.position).normalized;
+        Vector3 force = direction * moveSpeed * Time.deltaTime;
+        transform.position += force;
+
+        float distance = Vector2.Distance(transform.position, path.vectorPath[currentWP]);
+        if (distance < nextWaypointDistance)
+        {
+            currentWP++;
+        }
+
+        // Xác định hướng xoay mặt dựa trên direction thay vì force
+        if (Mathf.Abs(direction.x) > 0.01f)
+        {
+            zombieSR.transform.localScale = new Vector3(Mathf.Sign(direction.x) * 2, 2, 1);
+        }
+
+        yield return null;
+    }
+}
+
+}
